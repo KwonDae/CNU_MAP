aca60f465d081d03725227ee227e287aa9d86c1c
KwonDae <goodday94@cnu.ac.kr>
2017-11-21 17:18:55 +0900

    Term_project

--- app/src/main/java/com/example/daewon/cnu_map/MainActivity.java
+++ app/src/main/java/com/example/daewon/cnu_map/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //MainActivity
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
